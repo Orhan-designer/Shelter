15c0dffd7fc1d8ecfa749b617277ec4822b87589
Orhan <ya.eskimo-callboy@yandex.ru>
2021-02-20 01:03:49 +0300

    change path

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById("trigger").onclick = function() {open()};
+
+function open() {
+    document.getElementById("menu").classList.toggle("show");
+}
+
+/*var x = document.getElementById("contain");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav");
+  element.classList.toggle("open");
+  
+  x.classList.toggle("change");
+}*/
